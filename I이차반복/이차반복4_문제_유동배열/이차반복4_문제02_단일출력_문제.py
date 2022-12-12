58c8422d7fa38cd4fa9cb0bec4842c4464f98c5c
[K3YOU] <[7592667@naver.com]>
2022-12-12 18:10:28 +0900

    1212-1

--- I이차반복/이차반복4_문제_유동배열/이차반복4_문제02_단일출력_문제.py
+++ I이차반복/이차반복4_문제_유동배열/이차반복4_문제02_단일출력_문제.py
@@ -5,5 +5,29 @@
 	[정답]
 		1 2 3 4 100		
 '''
-a = [1,1,2,2,3,3,4,100,3]
+a = [1,3,9,2,2,2,3,]
+# 1 , 3, 9,''' 2, 
+'''
+if len(a) = 3, and  i is last turn, j is going to be same with len(a) = 3 (j==3) 
+for i in range(len(a)):
+	check = False
+	for j in range(i+1):# 1 , 2, 3 => 0 , 01 , 012
+		if ( i != j and a[i] == a[j]):  # 다른 인덱스 넘버지만 같은 수일 때를 표현한 것
+			check = True
+			break
 
+	if check == False :
+		print(a[i],end = " ")
+'''
+
+#앞에만 검사해서 중복이 있으면 나오게 하는거.
+
+for i in range(len(a)):
+	check = False
+	for j in range(i):# 1 , 2, 3 => 0 , 01 , 012
+		if (  a[i] == a[j]):  # 다른 인덱스 넘버지만 같은 수일 때를 표현한 것
+			check = True
+			break
+
+	if check == False :
+		print(a[i],end = " ")
--- I이차반복/이차반복4_문제_유동배열/이차반복4_문제06_석차정렬_문제.py
+++ I이차반복/이차반복4_문제_유동배열/이차반복4_문제06_석차정렬_문제.py
@@ -8,5 +8,10 @@
 numList = [1001, 1002, 1003, 1004]
 scoreList = [87, 42,  11, 98]
 
-for i in range(scoreList):
-	
+for i in range(len(scoreList)):
+	count = 0
+
+	for  j in range(len(scoreList)):
+		if scoreList[i]<=scoreList[j]:
+			count += 1
+	print(count,end=" ")
